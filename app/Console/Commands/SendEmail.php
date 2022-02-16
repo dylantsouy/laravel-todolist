888bdb06df91f89d67d42125bf3c7fab18566ce0
dylan.tsou <dylan.tsou@vivicloud.net>
2022-02-16 10:01:35 +0800

    add email task schdule

--- app/Console/Commands/SendEmail.php
+++ app/Console/Commands/SendEmail.php
@@ -3,6 +3,11 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\NotifyMail;
+use App\Services\TodoService;
+use App\Services\UserService;
 
 class SendEmail extends Command
 {
@@ -25,9 +30,13 @@ class SendEmail extends Command
      *
      * @return void
      */
-    public function __construct()
+    private $todoService;
+    private $userService;
+    public function __construct(TodoService $todoService, UserService $userService)
     {
         parent::__construct();
+        $this->todoService = $todoService;
+        $this->userService = $userService;
     }
 
     /**
@@ -37,6 +46,19 @@ class SendEmail extends Command
      */
     public function handle()
     {
-        return 0;
+        $todos = $this->todoService->getDeadline();
+        foreach ($todos as $todo) {
+            $deadline = $todo->deadline;
+            $name = $todo->user_name;
+            $user = $this->userService->getUserByName($name);
+            // One Day Later
+            if (Carbon::tomorrow()->toDateString() === Carbon::parse($deadline)->toDateString()) {
+                Mail::to($user->email)->send(new NotifyMail(1, $todo));
+            }
+            // Five Days Later
+            if (Carbon::now()->subDays(-5)->toDateString() === Carbon::parse($deadline)->toDateString()) {
+                Mail::to($user->email)->send(new NotifyMail(5, $todo));
+            }
+        }
     }
 }
